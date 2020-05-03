f8396c5adc9e2e8e165f5b206d486cfdcdf52493
alexmoralex <alejandromo1995@gmail.com>
2020-05-03 09:18:19 -0500

    Updates

---
+++ precache-manifest.6107fcd9dc90ea748be83d3765de0931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0dbea9277949f9816157997c50574d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2d4ce9c73113df10a75e",
+    "url": "/robofriends/static/css/2.a74af5d5.chunk.css"
+  },
+  {
+    "revision": "00a3c5e2efd54d016545",
+    "url": "/robofriends/static/css/main.77dc1c0e.chunk.css"
+  },
+  {
+    "revision": "2d4ce9c73113df10a75e",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00a3c5e2efd54d016545",
+    "url": "/robofriends/static/js/main.a04af460.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
